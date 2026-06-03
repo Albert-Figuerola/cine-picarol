6f159e3998cac8e1cdd37b789b8e86974b075673
Albert <afiguerola@gaddexapp.com>
2026-06-03 07:10:36 +0200

    Added create movie validations and testing

--- src/main/kotlin/com/albert/cinepicarol/movie/MovieService.kt
+++ src/main/kotlin/com/albert/cinepicarol/movie/MovieService.kt
@@ -7,6 +7,20 @@ class MovieService (
     private val movieRepository: MovieRepository
 ) {
 
-    fun createMovie(movie: MovieEntity): MovieEntity  = movieRepository.save(movie)
+    fun createMovie(movie: MovieEntity): MovieEntity  {
+        require(movie.title.isNotBlank()) {
+            "Movie title cannot be empty"
+        }
+
+        require(movie.description.isNotBlank()) {
+            "Movie description cannot be empty"
+        }
+
+        require(movie.durationMinutes > 0) {
+            "Movie duration minutes must be greater than zero"
+        }
+
+        return movieRepository.save(movie)
+    }
 
 }
