7d48cbbd8279922632e7fb738f10198fbf6b8170
Albert <afiguerola@gaddexapp.com>
2026-06-05 09:44:03 +0200

    Adding pagination to get movies

--- src/main/kotlin/com/albert/cinepicarol/movie/mapper/MovieMapper.kt
+++ src/main/kotlin/com/albert/cinepicarol/movie/mapper/MovieMapper.kt
@@ -2,14 +2,25 @@ package com.albert.cinepicarol.movie.mapper
 
 import com.albert.cinepicarol.movie.MovieEntity
 import com.albert.cinepicarol.movie.response.MovieResponse
+import com.albert.cinepicarol.movie.response.MoviesPageResponse
+import org.springframework.data.domain.Page
 
 internal fun MovieEntity.toResponse() = MovieResponse(
-        id,
-        title,
-        description,
-        releaseYear,
-        durationMinutes
+    id,
+    title,
+    description,
+    releaseYear,
+    durationMinutes
 )
 
-internal fun List<MovieEntity>.toResponse(): List<MovieResponse> =
-        map { it.toResponse() }
+
+internal fun Page<MovieEntity>.toResponse(): MoviesPageResponse =
+    MoviesPageResponse(
+        movies = content.map { it.toResponse() },
+        currentPage = number,
+        pageSize = size,
+        totalPages = totalPages,
+        totalElements = totalElements,
+        hasNext = hasNext(),
+        hasPrevious = hasPrevious()
+    )
